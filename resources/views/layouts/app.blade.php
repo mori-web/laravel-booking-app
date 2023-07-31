7ed489da56abb1b0cc91ef817781f843377af686
mori_web <minipla0318@gmail.com>
2023-07-31 09:49:57 +0900

    修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,9 +13,9 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
         <!-- Scripts -->
         @vite(['resources/css/app.css','resources/css/style.css', 'resources/js/app.js'])
+        {{-- @vite(['resources/css/style.css', 'resources/js/app.js']) --}}
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 content">
