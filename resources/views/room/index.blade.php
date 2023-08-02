270681cea5ff52414516054157915f6f38f06754
mori_web <minipla0318@gmail.com>
2023-08-02 23:59:51 +0900

    修正

--- resources/views/room/index.blade.php
+++ resources/views/room/index.blade.php
@@ -3,5 +3,7 @@
 @section('title', '客室紹介ページ')
 
 @section('content')
-  <img src="{{ asset('/build/assets/images/double_room.jpg') }}" alt="">
+  <img src="{{ asset('images/single_room.jpg') }}" alt="">
+  <img src="{{ asset('images/double_room.jpg') }}" alt="">
+  <img src="{{ asset('images/twin_room.jpg') }}" alt="">
 @endsection
